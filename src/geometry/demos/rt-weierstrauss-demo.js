b718eda0e9614127dac4c9902b36ce8b1f5c8a69
Andrew Thomson <andy@openbuilding.ca>
2026-01-02 15:40:51 -0500

    Revert "Fix: Float circle upward to prevent formula panel clipping"
    This reverts commit 3941881a2d3fff7dab2fc78ec2f1fbd557044bd7.

--- src/geometry/demos/rt-weierstrauss-demo.js
+++ src/geometry/demos/rt-weierstrauss-demo.js
@@ -62,9 +62,6 @@ export function initWeierstrassDemo() {
 
   ({ scene, camera, renderer, animate, cleanup } = sceneData);
 
-  // Move camera up to prevent formula panel from clipping circle
-  camera.position.y = 0.3;
-
   // Create visual elements
   createAxes();
   createCircle();
